f33d154c765d6ba230596e36b6c6c4120fdbe391
BuildTools <unconfigured@null.spigotmc.org>
2025-06-10 23:20:07 -0400

    setting up camera

--- forklift/cameraStream.cpp
+++ forklift/cameraStream.cpp
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main()
 {
+        cv::VideoCapture cap(0);
+        cv::Mat frame;
 
-std::cout << "hello";
-return 0;
+        cap >> frame;
+
+        cv::imwrite("captured_image.jpg", frame);
+
+        cap.release();
+
+        std::cout << "hello";
+        return 0;
 
 }
